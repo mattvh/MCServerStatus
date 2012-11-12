cdcbc465f098fdb46126c778c8850b68be154cb5
redwallhp <matt@ntugo.com>
2012-11-12 18:44:01 -0500

    Fixed for the new protocol in Minecraft 1.4. Also removed the exception thrown when a socket cannot be opened to the server, as the server being down isn't really...exceptional. It's just one of the expected states.

--- MCServerStatus/Minecraft/Stats.php
+++ MCServerStatus/Minecraft/Stats.php
@@ -4,36 +4,38 @@ namespace Minecraft;
 
 class Stats {
 
-	public static function retrieve( \Minecraft\Server $server )
-	{
+
+	public static function retrieve( \Minecraft\Server $server ) {
+
 		$socket = stream_socket_client(sprintf('tcp://%s:%u', $server->getHostname(), $server->getPort()), $errno, $errstr, 1);
 
-		if(!$socket) {
-			throw new StatsException("Could not connect to the Minecraft server.");
+		if (!$socket) {
+			$stats->is_online = false;
+			return $stats;
 		}
 
-		fwrite($socket, "\xfe");
-		
-		$data = fread($socket, 256);
-		
+		fwrite($socket, "\xfe\x01");
+		$data = fread($socket, 1024);
 		fclose($socket);
 
 		$stats = new \stdClass;
 
 		// Is this a disconnect with the ping?
-		if($data[0] !== "\xFF") {
+		if($data == false AND substr($data, 0, 1) != "\xFF") {
 			$stats->is_online = false;
 			return $stats;
 		}
 
-		$data = substr($data, 3);
+		$data = substr($data, 9);
 		$data = mb_convert_encoding($data, 'auto', 'UCS-2');
-		$data = explode("\xA7", $data);		
+		$data = explode("\x00", $data);
 
 		$stats->is_online = true;
-		list($stats->motd, $stats->online_players, $stats->max_players) = $data;
+		list($stats->protocol_version, $stats->game_version, $stats->motd, $stats->online_players, $stats->max_players) = $data;
 
 		return $stats;
+
 	}
+
 
 }
